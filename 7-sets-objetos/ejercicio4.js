f60f44076ca48fafc9bd972413e9ab7ec16e9a63
Jorge <JorgeMac@Jorges-MacBook-Pro.local>
2023-03-26 22:59:26 +0300

    11 Ejercicio 4 About:Blank, terminado

--- 7-sets-objetos/ejercicio4.js
+++ 7-sets-objetos/ejercicio4.js
@@ -1,9 +1,8 @@
 // Abre una nueva ventana "about:blank" en Google Chrome
-
+// --> Listo
 
 // Abre la consola de desarrollador de Google Chrome (F12)
-
+// --> Listo
 
 // Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
-
-
+// --> Listo
